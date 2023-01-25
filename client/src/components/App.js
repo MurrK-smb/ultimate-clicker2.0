1a815d56600d563a895ca501c2b2cc58928f391a
NicolasGil <nick.gil360@gmail.com>
2023-01-24 20:06:13 -0500

    set up frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Navbar/Navbar.js";
+import Game from "./Game/Game.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import "./Navbar.css";
+import React from "react";
+
+function Navbar() {
+  return <nav className="navbar"></nav>;
+}
+
+export default Navbar;
