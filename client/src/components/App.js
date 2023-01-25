79c8e20dfb4c9b245c118134f228119f19b0bf29
NicolasGil <nick.gil360@gmail.com>
2023-01-25 07:37:11 -0500

    create login page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header/Header.js";
 import Main from "./Main/Main.js";
+import Login from "./Login/Login.js";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,15 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__form-container">
+        <form className="login__form">
+          <h2>Login</h2>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
