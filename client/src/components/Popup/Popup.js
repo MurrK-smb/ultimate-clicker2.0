4673c3801e4928c89480a0c8d5d7cf241152ed0b
NicolasGil <nick.gil360@gmail.com>
2023-02-02 22:49:14 -0500

    move sign up logic to Register.js

--- client/src/components/Popup/Popup.js
+++ client/src/components/Popup/Popup.js
@@ -1,48 +1,18 @@
 import "./Popup.css";
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
-
-const USER_REGEX = /^[A-Za-z][A-Za-z0-9_-]{2,21}$/;
-const PASS_REGEX =
-  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{6,24}/;
 
 function Popup(props) {
   const navigate = useNavigate();
   const userRef = useRef();
-  // const [userFocus, setUserFocus] = useState(false);
-
-  const [user, setUser] = useState("");
-  const [validName, setValidName] = useState(false);
-
-  const [password, setPassword] = useState("");
-  const [validPassword, setValidPassword] = useState(false);
-
-  const [confirm, setConfirm] = useState("");
-  const [validConfirm, setValidConfirm] = useState(false);
-
-  const [errorMessage, setErrorMessage] = useState("");
-  const [success, setSuccess] = useState("");
 
   useEffect(() => {
     userRef.current.focus();
   }, []);
 
-  useEffect(() => {
-    const result = USER_REGEX.test(user);
-    setValidName(result);
-  }, [user]);
-
-  useEffect(() => {
-    const result = PASS_REGEX.test(password);
-    setValidPassword(result);
-
-    const match = password === confirm;
-    setValidConfirm(match);
-  }, [password, confirm]);
-
-  useEffect(() => {
-    setErrorMessage("");
-  }, [user, password, confirm]);
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <>
@@ -54,69 +24,81 @@ function Popup(props) {
             onClick={() => navigate("/")}
           ></button>
           <h2 className="popup__form-title">{props.data.title}</h2>
-          <form className="popup__form">
+          <form className="popup__form" onSubmit={handleSubmit}>
             <input
               className="popup__form-input"
               type="text"
               ref={userRef}
-              onChange={(e) => setUser(e.target.value)}
+              onChange={props.data.handleUserChange}
               required
-              // aria-invalid={validName ? "false" : "true"}
               placeholder="Username"
               autoComplete="off"
             />
-            <p className={user && !validName ? "error-message" : "hidden"}>
-              Must be from 3 - 20 characters long. Must begin with a letter.
-              0-9, _, - allowed.
-            </p>
+            {props.data.newUser && (
+              <p
+                className={
+                  props.data.user && !props.data.validName
+                    ? "error-message"
+                    : "hidden"
+                }
+              >
+                Must be from 3 - 20 characters long. Must begin with a letter.
+                0-9, _, - allowed.
+              </p>
+            )}
             <input
               className="popup__form-input"
               type="password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={props.data.handlePasswordChange}
               required
               placeholder="Password"
               autoComplete="off"
             />
-            <p
-              className={
-                password && !validPassword ? "error-message" : "hidden"
-              }
-            >
-              Must be 8 - 24 characters. Must include upper and lowercase
-              letters and include one special character
-            </p>
             {props.data.newUser && (
               <>
+                <p
+                  className={
+                    props.data.password && !props.data.validPassword
+                      ? "error-message"
+                      : "hidden"
+                  }
+                >
+                  Must be 8 - 24 characters. Must include upper and lowercase
+                  letters and include one special character
+                </p>
                 <input
                   className="popup__form-input"
                   type="password"
-                  onChange={(e) => {
-                    setConfirm(e.target.value);
-                  }}
+                  onChange={props.data.handleConfirmChange}
                   required
                   placeholder="Confirm Password"
                   autoComplete="off"
                 />
                 <p
                   className={
-                    confirm && !validConfirm ? "error-message" : "hidden"
+                    props.data.confirm && !props.data.validConfirm
+                      ? "error-message"
+                      : "hidden"
                   }
                 >
                   Must match password
                 </p>
               </>
             )}
-            {/* <p
-              ref={errorRef}
-              className={errorMessage ? "popup__error-message" : "hidden"}
-            >
-              {errorMessage}
-            </p> */}
             <button
-              className="popup__form-submit"
+              className={`popup__form-submit ${
+                props.data.validName &&
+                props.data.validPassword &&
+                props.data.validConfirm &&
+                "popup__form-submit_a"
+              }`}
               type="submit"
               disabled={
-                !validName || !validPassword || !validConfirm ? true : false
+                !props.data.validName ||
+                !props.data.validPassword ||
+                !props.data.validConfirm
+                  ? true
+                  : false
               }
             >
               <span className="popup__form-submit-text">
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,13 +1,62 @@
 import "./Register.css";
 import Popup from "../Popup/Popup.js";
+import { useState, useEffect } from "react";
 
 function Register() {
+  const USER_REGEX = /^[A-Za-z][A-Za-z0-9_-]{2,21}$/;
+  const PASS_REGEX =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{6,24}/;
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+
+  const [confirm, setConfirm] = useState("");
+  const [validConfirm, setValidConfirm] = useState(false);
+
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [success, setSuccess] = useState("");
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PASS_REGEX.test(password);
+    setValidPassword(result);
+
+    const match = password === confirm;
+    setValidConfirm(match);
+  }, [password, confirm]);
+
+  // useEffect(() => {
+  //   setErrorMessage("");
+  // }, [user, password, confirm]);
+
   const data = {
     title: "Welcome!",
     submit: "Sign Up",
     text: "Already have an account? Login ",
     pathname: "/login",
     newUser: true,
+    handleUserChange: (e) => {
+      setUser(e.target.value);
+    },
+    handlePasswordChange: (e) => {
+      setPassword(e.target.value);
+    },
+    handleConfirmChange: (e) => {
+      setConfirm(e.target.value);
+    },
+    user: user,
+    password: password,
+    confirm: confirm,
+    validName: validName,
+    validPassword: validPassword,
+    validConfirm: validConfirm,
   };
 
   return <Popup data={data} />;
